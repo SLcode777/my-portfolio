e5c655e23f629629e35589230a51443343c15c4d
SLcode777 <sl.code.777@gmail.com>
2025-12-18 17:43:32 +0100

    added og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,26 @@ const jetbrainsMono = JetBrains_Mono({
 export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [
+      {
+        url: "/og-img.png",
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: ["/og-img.png"],
+  },
 };
 
 export default function RootLayout({
